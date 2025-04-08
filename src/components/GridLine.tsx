cb23a85a1d535215aaf45dab459c332d49975263
leo9iota <leo9iota@gmail.com>
2025-04-08 17:47:43 +0200

    Update `GridLine` component with mesh and material properties

---
+++ src/components/GridLine.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type GridLineProps = JSX.IntrinsicElements['mesh'];
+
+function GridLine(props: GridLineProps) {
+  return (
+    <mesh {...props}>
+      <boxGeometry args={[0.05, 7, 0.15]} />
+      <meshStandardMaterial metalness={1} roughness={0.1} color={0x75aec4} />
+    </mesh>
+  );
+}
+
+export default React.memo(GridLine);
