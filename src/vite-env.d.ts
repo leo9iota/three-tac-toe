97d8575bf82e914c1dd8d9f833ed6af5adacb99e
leo9iota <leo9iota@gmail.com>
2025-04-08 18:05:38 +0200

    Revert "Remove HDR module declaration"
    This reverts commit dc89faea077c20ab08695c7d6e28ea773ea90b91.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,3 +3,8 @@
 /// <reference types="vite/client" />
 /// <reference types="react" />
 /// <reference types="@react-three/fiber" />
+
+declare module '*.hdr' {
+    const src: string;
+    export default src;
+}
