f224f4993dd7a016ebbe94ff54b926f03fb1cae0
leo9iota <leo9iota@gmail.com>
2025-04-08 18:51:21 +0200

    Add `GridLine` components to the `Canvas` for grid visualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Environment } from '@react-three/drei'; // Import orbital controls from drei
+import GridLine from './components/GridLine';
 import space from './assets/hdri/space.hdr'; // Import background image
 
 const App = () => {
@@ -20,6 +21,12 @@ const App = () => {
         <pointLight position={[-150, 300, -300]} intensity={0.9} />
 
         <Environment background={true} files={space} />
+
+        {/* create grid */}
+        <GridLine position={[2.5, 0, 0]} />
+        <GridLine position={[0, 0, 0]} />
+        <GridLine position={[1.2, -1.2, 0]} rotation={[0, 0, Math.PI / 2]} />
+        <GridLine position={[1.2, 1.2, 0]} rotation={[0, 0, Math.PI / 2]} />
       </Canvas>
     </>
   );
