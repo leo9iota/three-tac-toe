07d361d8cdabcb008854bc21b55443f0e3fdd43b
leo9iota <leo9iota@gmail.com>
2025-04-08 17:46:08 +0200

    Add TypeScript triple-slash directives - `vite/client` - `react` - `@react-three/fiber`

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,8 @@
 // vite-env.d.ts
+/* Add vite/client, react, and @react-three/fiber directives so TS  */
 /// <reference types="vite/client" />
+/// <reference types="react" />
+/// <reference types="@react-three/fiber" />
 
 declare module '*.hdr' {
     const src: string;
