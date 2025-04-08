dc89faea077c20ab08695c7d6e28ea773ea90b91
leo9iota <leo9iota@gmail.com>
2025-04-08 17:59:39 +0200

    Remove HDR module declaration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,8 +3,3 @@
 /// <reference types="vite/client" />
 /// <reference types="react" />
 /// <reference types="@react-three/fiber" />
-
-declare module '*.hdr' {
-    const src: string;
-    export default src;
-}
